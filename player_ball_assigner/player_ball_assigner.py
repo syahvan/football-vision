f7e5905f7e6df9e624f2c1420307f4d33ac11be7
Syahvan <syahvanalviansyah@students.undip.ac.id>
2024-08-28 00:03:42 +0700

    edit max distance

--- player_ball_assigner/player_ball_assigner.py
+++ player_ball_assigner/player_ball_assigner.py
@@ -4,7 +4,7 @@ from utils import get_center_of_bbox, measure_distance
 
 class PlayerBallAssigner():
     def __init__(self):
-        self.max_player_ball_distance = 70
+        self.max_player_ball_distance = 30
     
     def assign_ball_to_player(self,players,ball_bbox):
         ball_position = get_center_of_bbox(ball_bbox)
